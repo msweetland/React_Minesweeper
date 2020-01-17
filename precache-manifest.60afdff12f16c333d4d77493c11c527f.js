9ecb6962c7ef9eaa309ab88fb45aad26e00edb69
msweetland <mjsweet@umich.edu>
2020-01-17 17:37:04 -0500

    Updates

---
+++ precache-manifest.60afdff12f16c333d4d77493c11c527f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbb1eb0a2fb9b2ef5272a1fab9614f3",
+    "url": "/Emojisweeper/index.html"
+  },
+  {
+    "revision": "2c6ba49058f88dc41522",
+    "url": "/Emojisweeper/static/css/main.01fd12bc.chunk.css"
+  },
+  {
+    "revision": "cf6f8860515e78fc0f2d",
+    "url": "/Emojisweeper/static/js/2.a80a4080.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Emojisweeper/static/js/2.a80a4080.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c6ba49058f88dc41522",
+    "url": "/Emojisweeper/static/js/main.39fc56b1.chunk.js"
+  },
+  {
+    "revision": "1fc94f4b8be41329ea22",
+    "url": "/Emojisweeper/static/js/runtime-main.34359a0e.js"
+  }
+]);
